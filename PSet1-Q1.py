a8bb4c2e96c28623f7619f53f505c219725f384f
Muhammad-Amin1 <32972284+Muhammad-Amin1@users.noreply.github.com>
2018-01-17 11:06:30 -0500

    Added Instructions
    - Added instructions and sample test case

--- PSet1-Q1.py
+++ PSet1-Q1.py
@@ -1,5 +1,10 @@
 
-# You do not need to setup input for this question. Code as if it's already setup.
+
+# ~~~~~~~Instructions~~~~~~~~
+#Assume s is a string of lower case characters.
+#Write a program that counts up the number of vowels contained in the string s.
+# Valid vowels are: 'a', 'e', 'i', 'o', and 'u'.
+# For example, if s = 'azcbobobegghakl', your program should print: Number of vowels: 5
 
 count=0
 for letter in s:
