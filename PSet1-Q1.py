52cb9f87a1499c623bcf1921b7cccc4237853158
Muhammad-Amin1 <32972284+Muhammad-Amin1@users.noreply.github.com>
2018-01-17 10:15:18 -0500

    Summary
    Problem Set 1-Q1. Passes 10/10 test cases.

---
+++ PSet1-Q1.py
@@ -0,0 +1,9 @@
+
+# You do not need to setup input for this question. Code as if it's already setup.
+
+count=0
+for letter in s:
+    if letter=='a' or letter=='e' or letter=='i' or letter=='o' or letter=='u':
+        count+=1
+print("Number of vowels: "+ str(count))
+
